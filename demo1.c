d88d819a4979b1ee5ecfa50e0a5953f881db4bf0
fresh-demo <tiwari.kush1221r@gmail.com>
2025-12-05 12:50:51 +0530

    merge complete

---
+++ demo1.c
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str[50]="harsh tiwari";
+    char harsh[50]= "solitude";
+    int a , b;
+    a=strcmp(str);
+    b=strcmp(harsh);
+    printf("str=%s a=%d ",str,a);
+}
---
+++ demo2.c
@@ -0,0 +1,35 @@
+/*#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,a[8];
+    
+    for(i=0;i<5;i++)
+    {
+        printf("Enter five numbers:");
+        scanf("%d",&a[i]);
+    }
+    
+
+    printf("show all the numbers:%d",a[i]);
+   
+}*/
+/*#include<stdio.h>
+
+void main()
+{ int i; int marks[3];
+    float sum=0,avg;
+    printf("Enter the numbers:");
+    for(i=0;i<3;i++)
+    {
+        scanf( "%d" ,&marks[i]);
+    }
+    for(i=0;i<3;i++)
+    {
+        sum=sum+marks[i];
+       
+    }
+    avg=sum/3;
+    printf("avg=%f",avg);
+    
+}*/
